232593329a5c9612024f7be00338003f6df43b4a
mcnamee <mcnamee@users.noreply.github.com>
2020-08-24 00:08:51 +0000

    deploy: 99b7e2b2d82dfe430235d0b230bff4a83ebf6b46

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e11e661960e586682975d7d6e5ff3294.js"
+  "/precache-manifest.509a5c103feb04c7fca6095081551fd3.js"
 );
 
 self.addEventListener('message', (event) => {
